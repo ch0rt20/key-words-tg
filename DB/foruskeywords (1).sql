e3d3f27ac5241bfb932d637a2f703402294c8f19
ch0rt20 <115170325+ch0rt20@users.noreply.github.com>
2023-10-19 21:38:10 +0800

    Add files via upload

---
+++ DB/foruskeywords (1).sql
@@ -0,0 +1,16 @@
+ALTER TABLE `Message` DROP FOREIGN KEY `Message_fk0`;
+
+ALTER TABLE `User` DROP FOREIGN KEY `User_fk0`;
+
+ALTER TABLE `User` DROP FOREIGN KEY `User_fk1`;
+
+ALTER TABLE `Form_Message` DROP FOREIGN KEY `Form_Message_fk0`;
+
+ALTER TABLE `Form_Message` DROP FOREIGN KEY `Form_Message_fk1`;
+
+ALTER TABLE `Form_Message` DROP FOREIGN KEY `Form_Message_fk2`;
+
+ALTER TABLE `Form_Message` DROP FOREIGN KEY `Form_Message_fk3`;
+
+ALTER TABLE `Form_Message` DROP FOREIGN KEY `Form_Message_fk4`;
+
