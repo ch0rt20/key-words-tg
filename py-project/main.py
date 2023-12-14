1d323ebd5e5ed86197520fd0589f0bb54dda9c27
ch0rt20 <smbdpleasehelp.me@yandex.com>
2023-12-14 21:11:55 +0800

    Updated python application

--- py-project/main.py
+++ py-project/main.py
@@ -1,22 +1,15 @@
-#
-# Copyright Aliaksei Levin (levlam@telegram.org), Arseny Smirnov (arseny30@gmail.com),
-# Pellegrino Prevete (pellegrinoprevete@gmail.com)  2014-2023
-#
-# Distributed under the Boost Software License, Version 1.0. (See accompanying
-# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-#
 from ctypes.util import find_library
 from ctypes import *
 import json
 import sys
 
-# load shared library
+# Загрузка dll библиотеки
 tdjson_path = 'C:/td/tdlib/bin/tdjson.dll'
 if tdjson_path is None:
     sys.exit("Can't find 'tdjson' library")
 tdjson = CDLL(tdjson_path)
 
-# load TDLib functions from shared library
+# загрузка функций TDLib из общей библиотеки
 _td_create_client_id = tdjson.td_create_client_id
 _td_create_client_id.restype = c_int
 _td_create_client_id.argtypes = []
@@ -39,12 +32,13 @@ _td_set_log_message_callback = tdjson.td_set_log_message_callback
 _td_set_log_message_callback.restype = None
 _td_set_log_message_callback.argtypes = [c_int, log_message_callback_type]
 
-# initialize TDLib log with desired parameters
+# инициализируем TDLib лог с нужными параметрами
 @log_message_callback_type
 def on_log_message_callback(verbosity_level, message):
     if verbosity_level == 0:
         sys.exit('TDLib fatal error: %r' % message)
 
+#
 def td_execute(query):
     query = json.dumps(query).encode('utf-8')
     result = _td_execute(query)
@@ -54,14 +48,13 @@ def td_execute(query):
 
 _td_set_log_message_callback(2, on_log_message_callback)
 
-# setting TDLib log verbosity level to 1 (errors)
+# устанавливаем 1-й уровень детализации журнала TDLib
 print(str(td_execute({'@type': 'setLogVerbosityLevel', 'new_verbosity_level': 1, '@extra': 1.01234})).encode('utf-8'))
 
 
-# create client
+# создаем клиент
 client_id = _td_create_client_id()
 
-# simple wrappers for client usage
 def td_send(query):
     query = json.dumps(query).encode('utf-8')
     _td_send(client_id, query)
@@ -72,27 +65,26 @@ def td_receive():
         result = json.loads(result.decode('utf-8'))
     return result
 
-# another test for TDLib execute method
 print(str(td_execute({'@type': 'getTextEntities', 'text': '@telegram /test_command https://telegram.org telegram.me', '@extra': ['5', 7.0, 'a']})).encode('utf-8'))
 
-# start the client by sending a request to it
+# запуск клиента, отправляя на него запрос
 td_send({'@type': 'getOption', 'name': 'version', '@extra': 1.01234})
 
-# main events cycle
+# цикл с основными событиями
 while True:
     event = td_receive()
     if event:
-        # process authorization states
+        # стадии процесса авторизации
         if event['@type'] == 'updateAuthorizationState':
             auth_state = event['authorization_state']
 
-            # if client is closed, we need to destroy it and create new client
+            # если клиент закрыт, нужно его отключить и создать новый
             if auth_state['@type'] == 'authorizationStateClosed':
                 break
 
-            # set TDLib parameters
-            # you MUST obtain your own api_id and api_hash at https://my.telegram.org
-            # and use them in the setTdlibParameters call
+            # устанавливаем параметры TDLib
+            # api_id и api_hash мы берем здесь: https://my.telegram.org
+            # вставляем сюда
             if auth_state['@type'] == 'authorizationStateWaitTdlibParameters':
                 td_send({'@type': 'setTdlibParameters',
                          'database_directory': 'tdlib',
@@ -105,40 +97,40 @@ while True:
                          'application_version': '1.0',
                          'enable_storage_optimizer': True})
 
-            # enter phone number to log in
+            # вход по номеру телефона
             if auth_state['@type'] == 'authorizationStateWaitPhoneNumber':
                 phone_number = input('Please enter your phone number: ')
                 td_send({'@type': 'setAuthenticationPhoneNumber', 'phone_number': phone_number})
 
-            # enter email address to log in
+            # вход по email
             if auth_state['@type'] == 'authorizationStateWaitEmailAddress':
                 email_address = input('Please enter your email address: ')
                 td_send({'@type': 'setAuthenticationEmailAddress', 'email_address': email_address})
 
-            # wait for email authorization code
+            # ожидание кода авторизации с мыла
             if auth_state['@type'] == 'authorizationStateWaitEmailCode':
                 code = input('Please enter the email authentication code you received: ')
                 td_send({'@type': 'checkAuthenticationEmailCode',
                          'code': {'@type': 'emailAddressAuthenticationCode', 'code' : code}})
 
-            # wait for authorization code
+            # ожидание кода авторизации
             if auth_state['@type'] == 'authorizationStateWaitCode':
                 code = input('Please enter the authentication code you received: ')
                 td_send({'@type': 'checkAuthenticationCode', 'code': code})
 
-            # wait for first and last name for new users
+            # вход по имени и фамилии
             if auth_state['@type'] == 'authorizationStateWaitRegistration':
                 first_name = input('Please enter your first name: ')
                 last_name = input('Please enter your last name: ')
                 td_send({'@type': 'registerUser', 'first_name': first_name, 'last_name': last_name})
 
-            # wait for password if present
+            # ожидание паспорта, если имеется
             if auth_state['@type'] == 'authorizationStateWaitPassword':
                 password = input('Please enter your password: ')
                 td_send({'@type': 'checkAuthenticationPassword', 'password': password})
 
-        # Запуск нового потока с передачей параметра event
+        # запуск нового потока с передачей параметра event для дальнейшей сортировки
 
-        # handle an incoming update or an answer to a previously sent request
         print(str(event).encode('utf-8'))
         sys.stdout.flush()
+
